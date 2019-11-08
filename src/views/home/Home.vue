b58a690512bb109076287c868b74a0e6eecae742
liuxv <1721518931@qq.com>
2019-11-08 08:05:43 +0800

    navbar

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,100 @@
 <template>
-  <div>首页</div>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banners="banners" />
+    <recommend-view :recommends="recommends" />
+    <feature-view />
+    <tab-control :titles="['流行','新款','精选']" />
+    <good-list :goods="goods['pop'].list" />
+  </div>
 </template>
 
 <script>
+// 导入公共组件
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl";
+import GoodList from "components/content/goods/GoodList";
+
+//导入子组件
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
+
+import { getHomeMultidata, getHomeGoods } from "network/home";
+
 export default {
-	name: "Home",
-}
+  name: "Home",
+  components: {
+    NavBar,
+    TabControl,
+    GoodList,
+    HomeSwiper,
+    RecommendView,
+    FeatureView
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+      // 一定要事先设计好保存商品数据的数据结构,eg:map-map-array
+      goods: {
+        pop: { page: 0, list: [] },
+        news: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
+      }
+    };
+  },
+  created() {
+    //1.请求多个数据
+    this.getHomeMultidata();
+
+    //2.请求商品数据
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
+  },
+
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+        // console.log(res);
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+        // console.log(this.banners);
+      });
+    },
+
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then(res => {
+        console.log(res);
+        // ...ES6新增特性，可拼接两个数组
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+      });
+    }
+  }
+};
 </script>
 
 <style>
+.home-nav {
+  /* 背景颜色 */
+  background-color: var(--color-tint);
+  /* 字体颜色 */
+  color: #fff;
 
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
+
+.tab-control {
+  position: sticky;
+  top: 44px;
+}
 </style>
